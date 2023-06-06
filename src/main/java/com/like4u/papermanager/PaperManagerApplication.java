91a512de6bad810e97a1330af2be95d9b629e4a4
exception <z15563451092@gmail.com>
2023-06-06 10:36:49 +0800

    Initial commit

---
+++ src/main/java/com/like4u/papermanager/PaperManagerApplication.java
@@ -0,0 +1,13 @@
+package com.like4u.papermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperManagerApplication.class, args);
+    }
+
+}
